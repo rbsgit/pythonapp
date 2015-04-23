3a1f0eddaab1ca8d524d40f59ff5c38760762418
rbsgit <rbsgit@rbs.com.br>
2015-04-23 14:34:41 -0300

    test 3

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world from tsuru"
+    return "Hello world, Python in PAAS"
 
 app.run(port=int(os.environ.get("PORT", "5000")), host="0.0.0.0")
